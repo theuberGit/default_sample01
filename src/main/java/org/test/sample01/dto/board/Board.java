9546164c774a03a440e50276c9d174930c49b283
In Sanghak <platinasnow@naver.com>
2018-09-11 09:13:01 +0900

    mybatis 연결

---
+++ src/main/java/org/test/sample01/dto/board/Board.java
@@ -0,0 +1,25 @@
+package org.test.sample01.dto.board;
+
+public class Board {
+	
+	private Integer idx;
+	private String title;
+
+	public Integer getIdx() {
+		return idx;
+	}
+
+	public void setIdx(Integer idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+}
